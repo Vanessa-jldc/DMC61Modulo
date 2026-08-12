416f03c36ce5c4952c25501651f7dc2d0e5e22d6
Vanessa-jldc <vanessajacay.data@gmail.com>
2026-08-12 19:11:51 +0200

    Refactor input handling for number range
    Removed initial and final value inputs from the main section and moved the final value input to the 'Módulo Listas' section.

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ st.title("Especialización Python for analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Vanessa Jacay")
 
-valor_inicial = st.number_input("Ingrese el valor inicial")
-valor_final = st.number_input("Ingrese el valor final")                            
-
-lista_numeros = list(range(int(valor_inicial), int(valor_final))) 
-st.write(lista_numeros)                    
-
 modulos = st.sidebar.selectbox ("seleccione un módulo",["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
 
 if modulos == "Módulo Listas":
@@ -19,7 +13,6 @@ if modulos == "Módulo Listas":
   valor_final = st.number_input("Ingrese el valor final")
 
   lista_numeros = list(range(int(valor_inicial), int(valor_final)))
-
   st.write(lista_numeros)
   
 elif modulos == "Módulo Arreglos":

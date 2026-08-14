65b571583fd53c84cffcc5cd54f51053121d1cb4
Vanessa-jldc <vanessajacay.data@gmail.com>
2026-08-14 14:07:29 +0200

    Add functionality for array module in Streamlit app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 st.title("Especialización Python for analytics")
 st.sidebar.title("Parámetros")
@@ -7,19 +8,29 @@ st.write("Elaborado por: Vanessa Jacay")
 st.image("Python_logo.png",width = 300)
 st.sidebar.image("DMC.png", width = 100)
 
+
 modulos = st.sidebar.selectbox ("seleccione un módulo",["Módulo Listas", "Módulo Arreglos", "Módulo Funciones"])
 
 if modulos == "Módulo Listas":
+  
   st.write("Bienvenido al módulo Listas")
+  
   valor_inicial = st.number_input("Ingrese el valor inicial")
 
   valor_final = st.number_input("Ingrese el valor final")
-
   lista_numeros = list(range(int(valor_inicial), int(valor_final)))
   st.write(lista_numeros)
   
 elif modulos == "Módulo Arreglos":
-  st.write("Bienvenido al módulo de Arreglos")
   
+  st.write("Bienvenido al módulo de Arreglos")
+
+cantidad_elementos = st.slider("Selecione la cantidad de elementos de su arreglo", 1,100)
+
+  cantidad_arreglo= np.arange(cantidad_elementos)
+
+  st.write(cantidad_arreglo)
+
+
 else:
   st.write("Bienvenido al módulo de Funciones")

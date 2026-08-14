8c2967e25bb599bef485e9698444dcbf4689cdcd
Vanessa-jldc <vanessajacay.data@gmail.com>
2026-08-14 14:08:13 +0200

    Fix indentation in app.py for slider element

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ elif modulos == "Módulo Arreglos":
   
   st.write("Bienvenido al módulo de Arreglos")
 
-cantidad_elementos = st.slider("Selecione la cantidad de elementos de su arreglo", 1,100)
+  cantidad_elementos = st.slider("Selecione la cantidad de elementos de su arreglo", 1,100)
 
   cantidad_arreglo= np.arange(cantidad_elementos)
 
